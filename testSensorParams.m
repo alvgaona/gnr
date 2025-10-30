52a08d28569ab462d279d2c64e881e5f25c88b3c
Lucas-Go-Vel <lucas.gomez.velayos@alumnos.upm.es>
2025-10-30 13:44:41 +0100

    Add dist testing for landmarks

--- testSensorParams.m
+++ testSensorParams.m
@@ -2,6 +2,9 @@ nMeasures=100;sensorName='LMS100';landmark=true;
 %TESTSENSORPARAMS Tests n sensor measures, and calculates average and
 %covariance of the measures
 center_measures=zeros(1,nMeasures);
+if landmark
+    dist_measures=zeros(1,nMeasures);
+end
 for i = 1:nMeasures
     if landmark ~= true
         meas=apoloGetLaserData(sensorName);
@@ -13,8 +16,9 @@ for i = 1:nMeasures
         end
         center_measures(i) = meas(round(size(meas,2)/2)); %get the forward facing laser
     else
-        meas=apoloGetLaserLandMarks(sensorName).angle;
-        center_measures(i)=meas(1);
+        meas=apoloGetLaserLandMarks(sensorName);
+        center_measures(i)=meas.angle(1);
+        dist_measures(i)=meas.distance(1);
     end
     %pause(0.5);
     apoloUpdate();
@@ -27,3 +31,7 @@ subplot(1,2,2),title("Measure distribution"),histogram(center_measures);
 xline(avg,'--r',{'Average','= '+string(avg)});
 
 cov = std(center_measures);
+if landmark
+    avgD=mean(dist_measures);
+    covD=std(dist_measures);
+end
